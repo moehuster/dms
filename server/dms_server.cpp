f6f4946ca8490bf439187dfb907697321678206b
moehuster <moehuster@gmail.com>
2014-03-12 00:06:45 +0800

    producter

--- server/dms_server.cpp
+++ server/dms_server.cpp
@@ -1,11 +1,7 @@
-#include "dms_data.h"
-#include <cstdio>
+#include "userdata.h"
 #include <unistd.h>
-#include <pthread.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-using namespace std;
-void* receive_data(void* par);
 
 int main(int argc, char *argv[])
 {
@@ -32,20 +28,10 @@ int main(int argc, char *argv[])
 			perror("accept");
 			return 3;
 		}
-		pthread_t pid;
-		pthread_create(&pid,0,receive_data,&afd);
+//		pthread_t pid;
+//		pthread_create(&pid,0,receive_data,&afd);
+		Producter prod(afd);
+		prod.start();
 	}
 	close(fd);
 }				/* ----------  end of function main  ---------- */
-
-void* receive_data(void* par)
-{
-	int fd=*((int*)par);
-	MatchedLogRec mlog;
-	for (;;){
-		int rfd=recv(fd,&mlog,sizeof(mlog),0);
-		if (rfd<=0) break;
-		printf("%d: %s:%s\n",fd,mlog.logname,mlog.logip);
-	}
-	return (void*)close(fd);
-}
